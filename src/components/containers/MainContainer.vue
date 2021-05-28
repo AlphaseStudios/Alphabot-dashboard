d1ed7c3ed3c81ba39c6823c07b81a58ca4c92a0c
Ilja Busch <ilja.ragnar@gmail.com>
2021-05-28 21:33:07 +0200

    responsiveness fixes

--- src/components/containers/MainContainer.vue
+++ src/components/containers/MainContainer.vue
@@ -45,17 +45,41 @@
         >
           <img src="@/assets/hamburger.svg" class="w-14 h-14 pb-3" />
         </button>
-        <div v-if="mobileNav" class="fixed h-full w-full z-50 bg-black top-0 pt-10 bg-opacity-75">
+        <div
+          v-if="mobileNav"
+          class="fixed h-full w-full z-50 bg-black top-0 pt-10 bg-opacity-75"
+        >
           <div
             class="bg-gray-800 fixed z-50 w-72 h-80 rounded m-auto left-0 right-0 overflow-scroll"
           >
             <div class="text-gray-400 text-2xl pt-5 flex-col space-y-2">
-              <button class="flex bg-red-400 bg-opacity-25 mx-4 p-2 rounded" @click="mobileNav = false">Dismiss</button>
+              <button
+                class="flex bg-red-400 bg-opacity-25 mx-4 p-2 rounded"
+                @click="mobileNav = false"
+              >
+                Dismiss
+              </button>
               <router-link
                 to="/"
                 class="flex focus:bg-blue-500 focus:bg-opacity-25 bg-blue-400 bg-opacity-25 mx-4 p-2 rounded"
-                >Home</router-link
+                @click.native="mobileNav = false"
               >
+                Home
+              </router-link>
+              <router-link
+                to="/dashboard/pick"
+                class="flex focus:bg-blue-500 focus:bg-opacity-25 bg-blue-400 bg-opacity-25 mx-4 p-2 rounded"
+                @click.native="mobileNav = false"
+              >
+                Dashboard
+              </router-link>
+              <router-link
+                to="/account"
+                class="flex focus:bg-blue-500 focus:bg-opacity-25 bg-blue-400 bg-opacity-25 mx-4 p-2 rounded"
+                @click.native="mobileNav = false"
+              >
+                Account
+              </router-link>
               <a
                 class="flex focus:bg-blue-500 focus:bg-opacity-25 bg-blue-400 bg-opacity-25 mx-4 p-2 rounded"
                 href="//alphase.com"
@@ -67,16 +91,6 @@
                 href="//github.com/AlphaseStudios/Alphabot-dashboard"
                 target="_blank"
                 >Github</a
-              >
-              <router-link
-                to="/account"
-                class="flex focus:bg-blue-500 focus:bg-opacity-25 bg-blue-400 bg-opacity-25 mx-4 p-2 rounded"
-                >Account</router-link
-              >
-              <router-link
-                to="/dashboard/pick"
-                class="flex focus:bg-blue-500 focus:bg-opacity-25 bg-blue-400 bg-opacity-25 mx-4 p-2 rounded"
-                >Dashboard</router-link
               >
             </div>
           </div>
